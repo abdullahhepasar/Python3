f3d0732e7b86d1f736ecdf5e43d29f73061fb790
Abdullah <abdullahhepasar@gmail.com>
2017-10-18 02:11:02 +0300

    Python 3 | Giriş

---
+++ 1/giris.py
@@ -0,0 +1,18 @@
+# giris.py - hepasar.com
+
+#Bu formatta da kullanılır.
+##Hangar = "P51", "Yak_9P", "Bf_109", "Spitfire", "G55"
+##          
+##for her_ucak in Hangar:
+##    print(her_ucak)
+
+##########################################################
+#Ana fonksiyon isimi
+def main():
+    Hangar = "P51", "Yak_9P", "Bf_109", "Spitfire", "G55"
+          
+    for her_ucak in Hangar:
+        print(her_ucak)
+
+#Fonksiyonu başlat
+main()
